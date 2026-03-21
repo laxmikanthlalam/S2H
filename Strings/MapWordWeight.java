dba313b872d1ed6d4e1be7a00f0b5055208b4acb
laxmikanthlalam <lalamlaxmikanth1@gmail.com>
2026-03-21 21:03:57 +0530

    Create MapWordWeight.java

---
+++ Strings/MapWordWeight.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            int sum = 0;
+
+            for (int j = 0; j < words[i].length(); j++) {
+                char ch = words[i].charAt(j);
+                sum += weights[ch - 'a'];
+            }
+
+            int mod = sum % 26;
+            char mappedChar = (char) ('z' - mod);
+
+            result.append(mappedChar);
+        }
+
+        return result.toString();
+    }
+}
