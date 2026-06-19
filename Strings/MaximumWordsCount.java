1c725e5629fc0e5eac844faf1da29a37d56105b7
laxmikanthlalam <lalamlaxmikanth1@gmail.com>
2026-06-19 23:38:37 +0530

    Refactor mostWordsFound method to use split approach
    Removed the first approach and retained the second approach for counting maximum words in sentences.

---
+++ Strings/MaximumWordsCount.java
@@ -0,0 +1,33 @@
+//1st approach
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        int maxWords = 0;
+
+        for (String sentence : sentences) {
+            int words = 1;
+
+            for (char ch : sentence.toCharArray()) {
+                if (ch == ' ') {
+                    words++;
+                }
+            }
+
+            maxWords = Math.max(maxWords, words);
+        }
+
+        return maxWords;
+    }
+}
+//2nd Approach
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        int maxWords = 0;
+
+        for (String sentence : sentences) {
+            int words = sentence.trim().split("\\s+").length;
+            maxWords = Math.max(maxWords, words);
+        }
+
+        return maxWords;
+    }
+}
