17ec47fd6b8c937100d3c97567abd858fdc6ac41
laxmikanthlalam <lalamlaxmikanth1@gmail.com>
2026-06-19 12:22:12 +0530

    Update DivideTwoIntegers.java

--- Math/DivideTwoIntegers.java
+++ Math/DivideTwoIntegers.java
@@ -1,1 +1,34 @@
+import java.util.Scanner;
 
+class Solution {
+    public int divide(int dividend, int divisor) {
+
+        // Handle overflow case
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        return dividend / divisor;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter dividend: ");
+        int dividend = sc.nextInt();
+
+        System.out.print("Enter divisor: ");
+        int divisor = sc.nextInt();
+
+        Solution obj = new Solution();
+
+        int result = obj.divide(dividend, divisor);
+
+        System.out.println("Quotient = " + result);
+
+        sc.close();
+    }
+}
