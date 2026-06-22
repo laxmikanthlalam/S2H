04508a3f60daab99f89d1c8fd29f17326ab651bb
laxmikanthlalam <lalamlaxmikanth1@gmail.com>
2026-06-22 10:32:04 +0530

    Update MedianOf2SortedArr.java

--- ArrayList/MedianOf2SortedArr.java
+++ ArrayList/MedianOf2SortedArr.java
@@ -1,40 +1,44 @@
-         {
+import java.util.*;
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int n = n1 + n2;
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        int i = 0, j = 0;
+
+        while (i < n1 && j < n2) {
+            if (nums1[i] <= nums2[j]) {
                 arr.add(nums1[i]);
                 i++;
-            }
-            else{
+            } else {
                 arr.add(nums2[j]);
                 j++;
             }
         }
-        while(i<n1)
-        {
+
+        while (i < n1) {
             arr.add(nums1[i]);
             i++;
-
         }
-        while(j<n2)
-        {
+
+        while (j < n2) {
             arr.add(nums2[j]);
             j++;
         }
-        double mide=0;
-        if(n%2==0)
-        {
-           int mid1 = n/2;
-           mide = (arr.get(mid1)+arr.get(mid1-1))/2.0;
-          
-           return mide;
- 
+
+        if (n % 2 == 0) {
+            int mid1 = n / 2 - 1;
+            int mid2 = n / 2;
+
+            return (arr.get(mid1) + arr.get(mid2)) / 2.0;
+        } else {
+            int mid = n / 2;
+            return arr.get(mid);
         }
-        else
-        {
-           int mid = n/2;
-           return arr.get(mid); 
-        }
-
-
-             
-
     }
 }
