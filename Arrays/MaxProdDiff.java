7a41c9cf43388891290c12ed5cb2cd9867c4a4d5
laxmikanthlalam <lalamlaxmikanth1@gmail.com>
2026-07-21 16:33:40 +0530

    Implement maxProductDifference method in Solution class

---
+++ Arrays/MaxProdDiff.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxProductDifference(int[] nums) {
+        int max1=Integer.MIN_VALUE,max2=Integer.MIN_VALUE,min1=Integer.MAX_VALUE,min2=Integer.MAX_VALUE;
+        for(int i:nums)
+        {
+            if(i>max1)
+            {
+                max2=max1;
+                max1=i;
+            }
+            else if(i>max2)
+            {
+                max2=i;
+            }
+            if(i<min1)
+            {
+                min2=min1;
+                min1=i;
+            }
+            else if(i<min2)
+            {
+                min2=i;
+            }
+        }
+        return (max1*max2)-(min1*min2);
+    }
+}
